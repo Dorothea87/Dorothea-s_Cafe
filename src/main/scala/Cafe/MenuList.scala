4d60e65634a95f30767df8e2d78e920b5f2c694f
Dorothea Toeller <dorothea.toeller@mac.home>
2025-03-04 10:26:27 +0000

    create case class premium item

--- src/main/scala/Cafe/MenuList.scala
+++ src/main/scala/Cafe/MenuList.scala
@@ -1,6 +1,17 @@
 package Cafe
 
 object MenuList {
+  val flatwhite = MenuItem("Flat white", 4.95)
+  val matchalatte = MenuItem("Matcha latte", 6.95)
+  val dorotea = MenuItem("Doro-tea", 2.95)
+  val baconbutty = MenuItem("Bacon butty", 5.55)
+  val salmonbagel = MenuItem("Salmon bagel", 7.95)
+  val fullenglish = MenuItem("Full English", 9.85)
+  val hallumifry = MenuItem("Hallumi fry-up", 8.95)
+  val croissant = MenuItem("Croissant", 4.95)
+  val painsuisse = MenuItem("Pain au suisse", 5.75)
+  val dreamcake = MenuItem("Dorothea's Dream cake", 4.75)
+
   val standardMenu: List[MenuItem] = List(
     flatwhite,
     matchalatte,
@@ -13,15 +24,4 @@ object MenuList {
     painsuisse,
     dreamcake
     )
-  val flatwhite = MenuItem("Flat white", 4.95)
-  val matchalatte = MenuItem("Matcha latte", 6.95)
-  val dorotea = MenuItem("Doro-tea", 2.95)
-  val baconbutty = MenuItem("Bacon butty", 5.55)
-  val salmonbagel = MenuItem("Salmon bagel", 7.95)
-  val fullenglish = MenuItem("Full English", 9.85)
-  val hallumifry = MenuItem("Hallumi fry-up", 8.95)
-  val croissant = MenuItem("Croissant", 4.95)
-  val painsuisse = MenuItem("Pain au suisse", 5.75)
-  val dreamcake = MenuItem("Dorothea's Dream cake", 4.75)
-
 }
