67b5f39b10e3a4562ca54744c6cb0f7b9b287b3f
Dorothea Toeller <dorothea.toeller@mac.home>
2025-03-04 17:16:45 +0000

    creates sealed traits and implements them in all files

--- src/main/scala/Cafe/MenuItem.scala
+++ src/main/scala/Cafe/MenuItem.scala
@@ -1,3 +1,13 @@
 package Cafe
 
-case class MenuItem(name: String, price: Double)
+case class MenuItem(name: String, price: Double, foodType: FoodType)
+
+sealed trait FoodType
+
+case object Drink extends FoodType
+
+case object ColdFood extends FoodType
+
+case object HotFood extends FoodType
+
+case object Premium extends FoodType
